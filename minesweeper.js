7d97e0270babefd982fdf5bc035ef1c6af8f43ea
Caz <cjpitcher@gmail.com>
2020-09-25 13:38:32 +1200

    Added word click to event listener

--- minesweeper.js
+++ minesweeper.js
@@ -63,8 +63,8 @@ function startGame () {
   for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i])
   }
-  document.addEventListener(checkForWin);
-  document.addEventListener("contextmenu", checkForWin);
+  document.addEventListener('click', checkForWin)
+  document.addEventListener("contextmenu", checkForWin)
   lib.initBoard()
 }
 
