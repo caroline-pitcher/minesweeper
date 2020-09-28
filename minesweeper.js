faf57aea63a1f4a41542a0df07e49c670610a42d
Caz <cjpitcher@gmail.com>
2020-09-28 17:38:19 +1300

    Updated cSM again

--- minesweeper.js
+++ minesweeper.js
@@ -64,10 +64,10 @@ function checkForWin () {
 function countSurroundingMines (cell) {
   var surroundingCells = lib.getSurroundingCells(cell.row, cell.col)
   let mineCount = 0
-  for(i = 0; i < surroundingCells.length; i++) {
-    if (surroundingCells[i].isMine === true) {
+    surroundingCells.forEach(surCell => { 
+    if (surCell.isMine === true) {
       mineCount++;
     }
-  }
+  })
   return mineCount
 }
